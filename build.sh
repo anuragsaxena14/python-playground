34baab305ade4fa7fd9109d3a87f1785060b34a0
anewraag <anurag.saxena14@gmail.com>
2024-10-18 18:35:50 +0530

    Created simple_api binary file
    Created simple_api binary file for easy deployment

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -e
+set -x
+
+python3 -m venv dist/venv
+source dist/venv/bin/activate
+python3 -m pip install -r requirements.txt
+python3 -m pip install -r build_requirements.txt
+shiv --site-packages venv/lib/python3.12/site-packages \
+	--compressed \
+	-o simple_api \
+	-e simple_api.__main__:main src/ \
+	--upgrade
