25cac45d8b85b4642ee3745617048b3e895f45df
anewraag <anurag.saxena14@gmail.com>
2024-10-18 18:08:17 +0530

    Added simple_api package
    Added simple_api package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='simple_api',
+      version='1.0',
+      description='A Simple API',
+      packages=find_packages(),
+      )
