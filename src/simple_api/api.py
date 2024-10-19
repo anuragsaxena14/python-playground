936e5ffcdd9a053eb22c656d53fcd350b03938ba
anewraag <anurag.saxena14@gmail.com>
2024-10-19 09:35:35 +0530

    Updated env properties
    updated env properties

--- src/simple_api/api.py
+++ src/simple_api/api.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    name = os.environ.get('DEV_NAME', "Neo")
+    message = f"Hello World, I am {name}!"
+    return {"Message": message}
